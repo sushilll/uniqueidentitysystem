9fac29a5f92bb4145550e774354e4f12a7e0ff1a
sushilll <sushil101094@gmail.com>
2017-04-12 14:20:13 +0530

    changed from UISforHeroku to Uniqueidentitysystem

---
+++ WebContent/js/custom.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Sushil on 26-02-2017.
+ */
+ //scroll to about div
+$("#about").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".middle").offset().top}, 'slow');
+});
+
+function checkSignupForm(form) {
+    if(form.pwd1.value != form.pwd2.value){
+        form.pwd2.focus();
+        return false;
+    }
+    return true;
+}
